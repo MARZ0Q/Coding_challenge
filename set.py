b774f9a03ff64a7270baac29b0c56b6be74ed0f6
coodyboy <coodyboy2@gmail.com>
2024-02-06 21:42:55 +0600

    Set added

---
+++ set.py
@@ -0,0 +1,9 @@
+
+def nodpli(array):
+    new_array = []
+    for item in array:
+        if(not item in new_array):
+            new_array.append(item)
+    return new_array
+
+# It even works with string!!!
